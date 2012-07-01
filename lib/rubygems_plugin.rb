4b797ca7d96db5f0f03d67c62e013e58a535eb28
Kyrylo Silin <kyrylosilin@gmail.com>
2012-07-01 20:10:16 +0300

    Be silent, when we can't load Pry
    There are situations, when a user deletes Pry through `gem uninstall
    pry`, but leaves `pry-theme` gem intalled. After this use case, a user
    faces the LoadError (cannot load such file -- pry) every time he/she
    executes a gem command.
    
    Signed-off-by: Kyrylo Silin <kyrylosilin@gmail.com>

--- lib/rubygems_plugin.rb
+++ lib/rubygems_plugin.rb
@@ -1,4 +1,9 @@
-require 'pry'
-require 'pry-theme'
+begin
+  require 'pry'
+  require 'pry-theme'
 
-PryTheme.install_gem_hooks
+  PryTheme.install_gem_hooks
+rescue LoadError
+  # Be silent, when we can't load Pry (when it's uninstalled).
+end
+
