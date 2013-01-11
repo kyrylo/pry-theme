32d7fb9571947ba8249df887fe3fad55564e576d
Kyrylo Silin <kyrylosilin@gmail.com>
2013-01-11 02:46:16 +0200

    Add comments
    And refactor #validate_attrs

--- lib/pry-theme/term.rb
+++ lib/pry-theme/term.rb
@@ -1,27 +1,42 @@
 module PryTheme
+  # Represents a terminal colour (not ANSI). Checks whether a number fits in its
+  # colour model.
   class TERM
+
+    # @return [Integer] the values are 8, 16 or 256
     attr_reader :color_model
 
+    # @param [Integer] value
+    # @param [Integer] color_model
     def initialize(value, color_model = 256)
       validate_attrs(value, color_model)
       @value = value
       @color_model = color_model
     end
 
+    # @return [String]
     def inspect
       "(TERM-#{ @color_model }: #{ @value })"
     end
 
+    # @return [Integer]
     def to_i
       @value
     end
 
     private
 
+    # @param [Integer] value
+    # @param [Integer] color_model
+    # @raise [ArgumentError] if the +value+ isn't a valid Integer (not in
+    #   +color_model+ range) or provided incorrect +color_model+
+    # @raise [TypeError] if the +value+ or the +color_model+ isn't an Integer
+    #   at all
+    # @return [void]
     def validate_attrs(value, color_model)
       fixnums = value.is_a?(Fixnum) && color_model.is_a?(Fixnum)
-      if fixnums
-        correct_term =
+      correct_term =
+        if fixnums
           case color_model
           when 256 then value.between?(0, 255)
           when 16  then value.between?(0, 15)
@@ -30,16 +45,19 @@ module PryTheme
                     'invalid color model for PryTheme::TERM#new(): ' \
                     "\"#{ color_model }\""
           end
-      end
+        end
 
       return true if fixnums && correct_term
+
       unless fixnums
         raise TypeError, "can't convert #{ value.class } into PryTheme::TERM"
       end
+
       unless correct_term
         raise ArgumentError,
-              %|invalid TERM number for PryTheme::TERM#new(): "#{ value }"|
+          %|invalid TERM number for PryTheme::TERM#new(): "#{ value }"|
       end
     end
+
   end
 end
