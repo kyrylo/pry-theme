b5a55284c80c2bbc1cd7edce269b94e1e187752e
Kyrylo Silin <kyrylosilin@gmail.com>
2013-01-23 17:44:25 +0200

    Add `Theme`

--- lib/pry-theme/theme.rb
+++ lib/pry-theme/theme.rb
@@ -1,44 +1,100 @@
 module PryTheme
+
+  # Raised when something goes wrong with Pry Theme themes. It's a general
+  # exception for everything that comes into collision in theme files.
+  ThemeError = Class.new(StandardError)
+
+  # @since 0.2.0
+  # @api private
+  # @see PryTheme::create
+  #
+  # Creates a new Pry Theme theme. This class is not meant for the direct
+  # instantiation. Use {PryTheme::create} instead.
   class Theme
 
-    attr_reader :scheme, :author, :description, :color_depth, :version, :name
+    DEFAULT_CONFIG = {
+      :name           => "prytheme-#{ rand(1_000_000_000) }",
+      :color_model    => 256,
+      :author         => 'Unknown Author',
+      :description    => '',
+    }
 
-    def initialize(theme_filename)
-      theme_file = Helper.pathify_theme(theme_filename)
+    # Matches against valid theme name. It must start with a letter. The letter
+    # case is not important.
+    VALID_NAME = /\A[A-z][A-z0-9-]*\z/i
 
-      if File.exists?(theme_file)
-        theme = YAML.load_file(theme_file)
-      else
-        raise NoThemeError, "#{theme_filename}.prytheme doesn't exist"
-      end
+    # @return [Theme::Definition] the heart of every theme: the colour
+    #   definitions
+    attr_reader :definition
 
-      meta = theme["meta"]
+    # @see PryTheme::create
+    def initialize(config = {}, &block)
+      @config = DEFAULT_CONFIG.merge(config)
+      @authors = [{ :name => @config[:author] }]
+      @default_author = true
 
-      @name        = meta["theme-name"]
-      @version     = meta["version"]
-      @color_depth = meta["color-depth"].to_i
+      validate_config
 
-      # Forbid too long descriptions.
-      if @description = meta["description"]
-        if (size = @description.size) > 80
-          raise ThemeDescriptionError, "Description of #{name} theme is too long (#{size}). Max size is 80 characters."
-        end
-      end
-
-      @author      = meta["author"]
-      @scheme      = theme["theme"]
+      instance_eval(&block)
     end
 
-    def method_missing(method_name, *args, &block)
-      if @scheme.has_key?(method_name.to_s)
-        @scheme[method_name.to_s]
-      else
-        super
+    def author(options = nil)
+      if options
+        if options[:name].length > 32
+          raise PryTheme::ThemeError,
+            "author's name must be no longer than 32 characters"
+        end
+
+        if @default_author
+          @default_author = false
+          @authors[0] = options
+        else
+          @authors << options
+        end
+      end
+      @authors
+    end
+
+    def description(text = nil)
+      if text
+        if text.length > 280
+          raise PryTheme::ThemeError,
+            "description must be no longer than 280 characters"
+        end
+        @config[:description] = text
+      end
+      @config[:description]
+    end
+
+    def define_theme(&block)
+      @definition = Definition.new(color_model, &block)
+    end
+
+    def name
+      @config[:name]
+    end
+
+    def color_model
+      @config[:color_model]
+    end
+
+    private
+
+    def validate_config
+      if name !~ VALID_NAME
+        raise PryTheme::ThemeError, 'theme name must start with a letter'
+      end
+
+      if name.length > 18
+        raise PryTheme::ThemeError,
+          'theme name must be no longer than 18 characters'
+      end
+
+      unless [256, 8, 16].include?(color_model)
+        raise PryTheme::ThemeError,
+          'incorrect color model. Available values: 8, 16 or 256'
       end
     end
 
   end
-
-  class NoThemeError < StandardError; end
-  class ThemeDescriptionError < StandardError; end
 end
