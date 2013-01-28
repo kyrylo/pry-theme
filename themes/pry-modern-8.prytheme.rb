3e4461c2a9924fc79051bf5d57c76c2017df78fc
Kyrylo Silin <kyrylosilin@gmail.com>
2013-01-28 13:31:10 +0200

    Add "pry-theme-8" theme

---
+++ themes/pry-modern-8.prytheme.rb
@@ -0,0 +1,48 @@
+t = PryTheme.create name: 'pry-modern-8', color_model: 8 do
+  author name: 'Kyrylo Silin', email: 'kyrylosilin@gmail.com'
+  description 'Simplied version of pry-modern-16'
+
+  define_theme do
+    class_ 'magenta'
+    class_variable 'cyan'
+    comment 'blue'
+    constant 'blue'
+    error 'black', 'white'
+    float 'red'
+    global_variable 'yellow'
+    inline_delimiter 'green'
+    instance_variable 'cyan'
+    integer 'blue'
+    keyword 'red'
+    method 'yellow'
+    predefined_constant 'cyan'
+    symbol 'green'
+
+    regexp do
+      self_ 'red'
+      char 'red'
+      content 'magenta'
+      delimiter 'red'
+      modifier 'red'
+      escape 'red'
+    end
+
+    shell do
+      self_ 'green'
+      char 'green'
+      content 'green'
+      delimiter 'green'
+      escape 'green'
+    end
+
+    string do
+      self_ 'green'
+      char 'green'
+      content 'green'
+      delimiter 'green'
+      escape 'green'
+    end
+  end
+end
+
+PryTheme::ThemeList.add_theme(t)
