44dd8542bc45c365b949f075ea9aad90584edb07
Kyrylo Silin <kyrylosilin@gmail.com>
2013-01-28 10:23:40 +0200

    Add pry-love-8 theme

---
+++ themes/pry-love-8.prytheme.rb
@@ -0,0 +1,48 @@
+t = PryTheme.create name: 'pry-love-8', color_model: 8 do
+  author name: 'Kyrylo Silin', email: 'kyrylosilin@gmail.com'
+  description 'Adds the love, expressed in 8 colors'
+
+  define_theme do
+    class_ 'magenta'
+    class_variable 'red'
+    comment 'cyan'
+    constant 'magenta'
+    error 'yellow', 'magenta'
+    float 'magenta'
+    global_variable 'red'
+    inline_delimiter 'red'
+    instance_variable 'red'
+    integer 'magenta'
+    keyword 'red'
+    method 'green'
+    predefined_constant 'magenta'
+    symbol 'magenta'
+
+    regexp do
+      self_ 'yellow'
+      char 'yellow'
+      content 'yellow'
+      delimiter 'red'
+      modifier 'yellow'
+      escape 'magenta'
+    end
+
+    shell do
+      self_ 'yellow'
+      char 'yellow'
+      content 'yellow'
+      delimiter 'red'
+      escape 'magenta'
+    end
+
+    string do
+      self_ 'yellow'
+      char 'yellow'
+      content 'yellow'
+      delimiter 'red'
+      escape 'magenta'
+    end
+  end
+end
+
+PryTheme::ThemeList.add_theme(t)
