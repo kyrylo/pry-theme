fe978eb5f00ba91b929d3279f56f7b96f99f00ac
Kyrylo Silin <kyrylosilin@gmail.com>
2013-01-28 09:55:39 +0200

    pry-classic-256: fix some colours

--- themes/pry-classic-256.prytheme.rb
+++ themes/pry-classic-256.prytheme.rb
@@ -3,35 +3,44 @@ t = PryTheme.create name: 'pry-classic-256' do
   description 'The default Pry Theme'
 
   define_theme do
-    class_ 'magenta'
-    class_variable 'cyan'
-    comment 'blue'
+    class_ 'pale_red_violet03', [:bold]
+    class_variable 'cerulean_grey02'
+    comment 'wisteria02'
     constant 'blue'
     error 'yellow', 'red'
     float 'magenta'
-    global_variable bg: 'green'
+    global_variable 'lime02'
+    inline_delimiter 'red'
+    instance_variable 'cerulean_grey02'
     integer 'blue'
     keyword 'red'
-    method 'blue'
-    predefined_constant 'cyan'
+    method 'blue', [:bold]
+    predefined_constant 'robin_egg_blue04'
     symbol 'green'
 
     regexp do
       self_ 'red'
-      content 'red'
-      delimiter 'red'
-      modifier 'magenta'
+      char 'pale_red_violet03'
+      content 'eggplant02'
+      delimiter 'red', [:bold]
+      modifier 'magenta', [:underline]
+      escape 'red'
     end
 
     shell do
-      self_ bg: 'green'
-      delimiter 'white'
+      self_ 'green'
+      char 'dark_spring_green'
+      content 'green'
+      delimiter 'green', [:bold]
+      escape 'dark_spring_green'
     end
 
     string do
       self_ 'green'
+      char 'dark_spring_green'
       content 'green'
-      delimiter 'green'
+      delimiter 'green', [:bold]
+      escape 'dark_spring_green'
     end
   end
 end
