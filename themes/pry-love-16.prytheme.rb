56b5b099023a1108b80872437dd431644b5a1fe5
Kyrylo Silin <kyrylosilin@gmail.com>
2013-01-28 12:57:33 +0200

    Add "pry-love-16" theme

---
+++ themes/pry-love-16.prytheme.rb
@@ -0,0 +1,48 @@
+t = PryTheme.create name: 'pry-love-16', color_model: 16 do
+  author name: 'Kyrylo Silin', email: 'kyrylosilin@gmail.com'
+  description 'Adds the love, expressed in 16 colors'
+
+  define_theme do
+    class_ 'bright_magenta'
+    class_variable 'red'
+    comment 'cyan'
+    constant 'magenta'
+    error 'yellow', 'magenta'
+    float 'magenta'
+    global_variable 'red'
+    inline_delimiter 'red'
+    instance_variable 'red'
+    integer 'magenta'
+    keyword 'red'
+    method 'bright_green'
+    predefined_constant 'magenta'
+    symbol 'magenta'
+
+    regexp do
+      self_ 'yellow'
+      char 'yellow'
+      content 'yellow'
+      delimiter 'bright_red'
+      modifier 'bright_yellow'
+      escape 'bright_magenta'
+    end
+
+    shell do
+      self_ 'yellow'
+      char 'yellow'
+      content 'yellow'
+      delimiter 'bright_red'
+      escape 'bright_magenta'
+    end
+
+    string do
+      self_ 'yellow'
+      char 'yellow'
+      content 'yellow'
+      delimiter 'bright_red'
+      escape 'bright_magenta'
+    end
+  end
+end
+
+PryTheme::ThemeList.add_theme(t)
