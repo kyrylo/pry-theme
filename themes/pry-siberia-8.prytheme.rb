2772d389ba8feeb0c360d12da6f7bb4bd5066e97
Kyrylo Silin <kyrylosilin@gmail.com>
2013-01-28 02:49:23 +0200

    Comment pry-siberia-8 definition

--- themes/pry-siberia-8.prytheme.rb
+++ themes/pry-siberia-8.prytheme.rb
@@ -1,43 +1,43 @@
----
-meta:
-  theme-name  : pry-cold
-  version     : 4
-  color-depth : 256
-  description : Based on Charcoalblack theme from Emacs
-  author      : John Mair,
-                Kyrylo Silin <kyrylosilin@gmail.com>
+#---
+#meta:
+  #theme-name  : pry-cold
+  #version     : 4
+  #color-depth : 256
+  #description : Based on Charcoalblack theme from Emacs
+  #author      : John Mair,
+                #Kyrylo Silin <kyrylosilin@gmail.com>
 
-theme:
-  class               : robin_egg_blue02 (b)
-  class_variable      : pale_blue03 (b)
-  comment             : light_grey03
-  constant            : robin_egg_blue02 (b)
-  error               : robin_egg_blue02 (bi)
-  float               : silver01
-  global_variable     : robin_egg_blue02
-  inline_delimiter    : puce01
-  instance_variable   : pale_blue03
-  integer             : seroburomalinovyj01
-  keyword             : pale_blue03 (b)
-  method              : royal_blue05
-  predefined_constant : robin_egg_blue02 (b)
-  regexp:
-    self              : gray02
-    char              : white
-    content           : bluish03
-    delimiter         : gray02
-    modifier          : gray02
-    escape            : puce01
-  shell:
-    self              : gray02
-    char              : white
-    content           : puce01
-    delimiter         : gray02
-    escape            : puce01
-  string:
-    self              : bluish03
-    char              : white
-    content           : bluish03
-    delimiter         : bluish03
-    escape            : puce01
-  symbol              : bluish03
+#theme:
+  #class               : robin_egg_blue02 (b)
+  #class_variable      : pale_blue03 (b)
+  #comment             : light_grey03
+  #constant            : robin_egg_blue02 (b)
+  #error               : robin_egg_blue02 (bi)
+  #float               : silver01
+  #global_variable     : robin_egg_blue02
+  #inline_delimiter    : puce01
+  #instance_variable   : pale_blue03
+  #integer             : seroburomalinovyj01
+  #keyword             : pale_blue03 (b)
+  #method              : royal_blue05
+  #predefined_constant : robin_egg_blue02 (b)
+  #regexp:
+    #self              : gray02
+    #char              : white
+    #content           : bluish03
+    #delimiter         : gray02
+    #modifier          : gray02
+    #escape            : puce01
+  #shell:
+    #self              : gray02
+    #char              : white
+    #content           : puce01
+    #delimiter         : gray02
+    #escape            : puce01
+  #string:
+    #self              : bluish03
+    #char              : white
+    #content           : bluish03
+    #delimiter         : bluish03
+    #escape            : puce01
+  #symbol              : bluish03
