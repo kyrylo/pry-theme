6692e1a609f54f12b89d3e07a620fe15a093fc50
Kyrylo Silin <kyrylosilin@gmail.com>
2013-01-28 10:03:27 +0200

    Add pry-monochrome

---
+++ themes/pry-monochrome.prytheme.rb
@@ -0,0 +1,32 @@
+t = PryTheme.create name: 'pry-monochrome' do
+  author name: 'Kyrylo Silin', email: 'kyrylosilin@gmail.com'
+  description 'For people who hate colors'
+
+  define_theme do
+    class_ [:bold, :underline]
+    comment [:italic]
+    constant [:bold]
+    error [:bold, :italic, :underline]
+    global_variable [:bold]
+    inline_delimiter [:bold]
+    keyword [:bold]
+    method [:underline]
+    predefined_constant [:italic]
+    symbol [:underline]
+
+    regexp do
+      delimiter [:bold]
+      modifier [:italic]
+    end
+
+    shell do
+      delimiter [:bold]
+    end
+
+    string do
+      delimiter [:bold]
+    end
+  end
+end
+
+PryTheme::ThemeList.add_theme(t)
