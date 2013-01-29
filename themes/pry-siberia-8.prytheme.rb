8628b132e133030aa52e763195255fdd0254b246
Kyrylo Silin <kyrylosilin@gmail.com>
2013-01-29 04:35:10 +0200

    Add "pry-siberia-8" theme

--- themes/pry-siberia-8.prytheme.rb
+++ themes/pry-siberia-8.prytheme.rb
@@ -1,43 +1,48 @@
-#---
-#meta:
-  #theme-name  : pry-cold
-  #version     : 4
-  #color-depth : 256
-  #description : Based on Charcoalblack theme from Emacs
-  #author      : John Mair,
-                #Kyrylo Silin <kyrylosilin@gmail.com>
+t = PryTheme.create name: 'pry-siberia-8', color_model: 8 do
+  author name: 'Kyrylo Silin', email: 'kyrylosilin@gmail.com'
+  description 'This one reminds me of the cold Siberia...'
 
-#theme:
-  #class               : robin_egg_blue02 (b)
-  #class_variable      : pale_blue03 (b)
-  #comment             : light_grey03
-  #constant            : robin_egg_blue02 (b)
-  #error               : robin_egg_blue02 (bi)
-  #float               : silver01
-  #global_variable     : robin_egg_blue02
-  #inline_delimiter    : puce01
-  #instance_variable   : pale_blue03
-  #integer             : seroburomalinovyj01
-  #keyword             : pale_blue03 (b)
-  #method              : royal_blue05
-  #predefined_constant : robin_egg_blue02 (b)
-  #regexp:
-    #self              : gray02
-    #char              : white
-    #content           : bluish03
-    #delimiter         : gray02
-    #modifier          : gray02
-    #escape            : puce01
-  #shell:
-    #self              : gray02
-    #char              : white
-    #content           : puce01
-    #delimiter         : gray02
-    #escape            : puce01
-  #string:
-    #self              : bluish03
-    #char              : white
-    #content           : bluish03
-    #delimiter         : bluish03
-    #escape            : puce01
-  #symbol              : bluish03
+  define_theme do
+    class_ 'blue'
+    class_variable 'blue'
+    comment 'yellow'
+    constant 'blue'
+    error 'white', 'blue'
+    float 'yellow'
+    global_variable 'yellow'
+    inline_delimiter 'green'
+    instance_variable 'blue'
+    integer 'yellow'
+    keyword 'cyan'
+    method 'cyan'
+    predefined_constant 'cyan'
+    symbol 'cyan'
+
+    regexp do
+      self_ 'blue'
+      char 'cyan'
+      content 'blue'
+      delimiter 'cyan'
+      modifier 'blue'
+      escape 'blue'
+    end
+
+    shell do
+      self_ 'blue'
+      char 'cyan'
+      content 'blue'
+      delimiter 'cyan'
+      escape 'blue'
+    end
+
+    string do
+      self_ 'blue'
+      char 'cyan'
+      content 'blue'
+      delimiter 'cyan'
+      escape 'blue'
+    end
+  end
+end
+
+PryTheme::ThemeList.add_theme(t)
