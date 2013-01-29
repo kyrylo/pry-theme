415b7206a6bf553f23199b4546d55d37e40a4982
Kyrylo Silin <kyrylosilin@gmail.com>
2013-01-29 05:27:30 +0200

    Add "pry-tepid-16" theme

---
+++ themes/pry-tepid-16.prytheme.rb
@@ -0,0 +1,48 @@
+t = PryTheme.create name: 'pry-tepid-16', color_model: 16 do
+  author name: 'Kyrylo Silin', email: 'kyrylosilin@gmail.com'
+  description 'Warm colors with warm soul'
+
+  define_theme do
+    class_ 'bright_magenta'
+    class_variable 'bright_magenta'
+    comment 'green'
+    constant 'bright_magenta'
+    error 'black', 'bright_magenta'
+    float 'bright_white'
+    global_variable 'bright_white'
+    inline_delimiter 'yellow'
+    instance_variable 'bright_magenta'
+    integer 'bright_white'
+    keyword 'yellow'
+    method 'bright_yellow'
+    predefined_constant 'bright_yellow'
+    symbol 'yellow'
+
+    regexp do
+      self_ 'bright_magenta'
+      char 'bright_yellow'
+      content 'bright_magenta'
+      delimiter 'magenta'
+      modifier 'yellow'
+      escape 'yellow'
+    end
+
+    shell do
+      self_ 'bright_magenta'
+      char 'bright_yellow'
+      content 'bright_magenta'
+      delimiter 'yellow'
+      escape 'yellow'
+    end
+
+    string do
+      self_ 'bright_magenta'
+      char 'bright_yellow'
+      content 'bright_magenta'
+      delimiter 'yellow'
+      escape 'yellow'
+    end
+  end
+end
+
+PryTheme::ThemeList.add_theme(t)
