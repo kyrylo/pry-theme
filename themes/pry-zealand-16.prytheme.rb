7964a91bc054282fa1b8de0666e9ae7b47fbc2cc
Kyrylo Silin <kyrylosilin@gmail.com>
2013-01-29 04:59:08 +0200

    Add "pry-zealand-16" theme

---
+++ themes/pry-zealand-16.prytheme.rb
@@ -0,0 +1,48 @@
+t = PryTheme.create name: 'pry-zealand-16', color_model: 16 do
+  author name: 'Kyrylo Silin', email: 'kyrylosilin@gmail.com'
+  description 'Feel the presence of green, ecologic and flourishing New Zealand in your terminal'
+
+  define_theme do
+    class_ 'bright_yellow'
+    class_variable 'bright_yellow'
+    comment 'white'
+    constant 'bright_yellow'
+    error 'bright_black', 'bright_yellow'
+    float 'bright_white'
+    global_variable 'bright_white'
+    inline_delimiter 'green'
+    instance_variable 'bright_yellow'
+    integer 'bright_white'
+    keyword 'green'
+    method 'bright_green'
+    predefined_constant 'bright_green'
+    symbol 'green'
+
+    regexp do
+      self_ 'bright_yellow'
+      char 'bright_green'
+      content 'bright_yellow'
+      delimiter 'yellow'
+      modifier 'green'
+      escape 'green'
+    end
+
+    shell do
+      self_ 'bright_yellow'
+      char 'bright_green'
+      content 'bright_yellow'
+      delimiter 'green'
+      escape 'green'
+    end
+
+    string do
+      self_ 'bright_yellow'
+      char 'bright_green'
+      content 'bright_yellow'
+      delimiter 'green'
+      escape 'green'
+    end
+  end
+end
+
+PryTheme::ThemeList.add_theme(t)
