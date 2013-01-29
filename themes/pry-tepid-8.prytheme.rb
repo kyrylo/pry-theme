2644240bdac8a78e1345b0db3276e101f3d12f4a
Kyrylo Silin <kyrylosilin@gmail.com>
2013-01-29 05:27:20 +0200

    Add "pry-tepid-8" theme

---
+++ themes/pry-tepid-8.prytheme.rb
@@ -0,0 +1,48 @@
+t = PryTheme.create name: 'pry-tepid-8', color_model: 8 do
+  author name: 'Kyrylo Silin', email: 'kyrylosilin@gmail.com'
+  description 'Warm colors with warm soul'
+
+  define_theme do
+    class_ 'magenta'
+    class_variable 'magenta'
+    comment 'green'
+    constant 'magenta'
+    error 'black', 'magenta'
+    float 'green'
+    global_variable 'green'
+    inline_delimiter 'yellow'
+    instance_variable 'magenta'
+    integer 'green'
+    keyword 'yellow'
+    method 'yellow'
+    predefined_constant 'yellow'
+    symbol 'yellow'
+
+    regexp do
+      self_ 'magenta'
+      char 'yellow'
+      content 'magenta'
+      delimiter 'magenta'
+      modifier 'yellow'
+      escape 'yellow'
+    end
+
+    shell do
+      self_ 'magenta'
+      char 'yellow'
+      content 'magenta'
+      delimiter 'yellow'
+      escape 'yellow'
+    end
+
+    string do
+      self_ 'magenta'
+      char 'yellow'
+      content 'magenta'
+      delimiter 'yellow'
+      escape 'yellow'
+    end
+  end
+end
+
+PryTheme::ThemeList.add_theme(t)
