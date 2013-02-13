0c61aa6e6312c1961843e36f2dfcca01ff762027
Kyrylo Silin <kyrylosilin@gmail.com>
2013-02-13 08:01:40 +0200

    Add Stylesheet class

---
+++ spec/editor/stylesheet_spec.rb
@@ -0,0 +1,31 @@
+require 'helper'
+
+describe PryTheme::Editor::Stylesheet do
+
+  it "defaults to some values" do
+    stylesheet = PryTheme::Editor::Stylesheet.new('.piglet')
+    stylesheet.css.should =~ /color: rgb\(0, 0, 0\)/
+    stylesheet.css.should =~ /background-color: rgb\(255, 255, 255\)/
+    stylesheet.css.should =~ /font-style: normal/
+    stylesheet.css.should =~ /font-weight: normal/
+    stylesheet.css.should =~ /text-decoration: none/
+  end
+
+  it "defines attributes" do
+    stylesheet = PryTheme::Editor::Stylesheet.new('.winnie .the .pooh') do |s|
+      s.color = PryTheme::RGB.new([213, 211, 44])
+      s.background_color = PryTheme::RGB.new([21, 0, 11])
+      s.font_style = 'italic'
+      s.font_weight = 'bold'
+      s.text_decoration = 'underline'
+    end
+
+    stylesheet.css.should =~ /.winnie .the .pooh/
+    stylesheet.css.should =~ /color: rgb\(213, 211, 44\)/
+    stylesheet.css.should =~ /background-color: rgb\(21, 0, 11\)/
+    stylesheet.css.should =~ /font-style: italic/
+    stylesheet.css.should =~ /font-weight: bold/
+    stylesheet.css.should =~ /text-decoration: underline/
+  end
+
+end
