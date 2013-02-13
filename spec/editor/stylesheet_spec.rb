4ddf3f60db6c78a24d0d1f62affcdc4ffe660355
Kyrylo Silin <kyrylosilin@gmail.com>
2013-02-13 18:18:07 +0200

    Editor::StylesheetSpec: stylesheets should not prepend dots

--- spec/editor/stylesheet_spec.rb
+++ spec/editor/stylesheet_spec.rb
@@ -3,12 +3,12 @@ require 'helper'
 describe PryTheme::Editor::Stylesheet do
 
   it "has selector" do
-    stylesheet = PryTheme::Editor::Stylesheet.new('.piglet')
-    stylesheet.selector.should == '.piglet'
+    stylesheet = PryTheme::Editor::Stylesheet.new('piglet')
+    stylesheet.selector.should == 'piglet'
   end
 
   it "defaults to some values" do
-    stylesheet = PryTheme::Editor::Stylesheet.new('.piglet')
+    stylesheet = PryTheme::Editor::Stylesheet.new('piglet')
     stylesheet.css.should =~ /color: rgb\(0, 0, 0\)/
     stylesheet.css.should =~ /background-color: transparent/
     stylesheet.css.should =~ /font-style: normal/
@@ -17,7 +17,7 @@ describe PryTheme::Editor::Stylesheet do
   end
 
   it "defines attributes" do
-    stylesheet = PryTheme::Editor::Stylesheet.new('.winnie .the .pooh') do |s|
+    stylesheet = PryTheme::Editor::Stylesheet.new('winnie .the .pooh') do |s|
       s.color = PryTheme::RGB.new([213, 211, 44])
       s.background_color = PryTheme::RGB.new([21, 0, 11])
       s.font_style = 'italic'
@@ -25,7 +25,7 @@ describe PryTheme::Editor::Stylesheet do
       s.text_decoration = 'underline'
     end
 
-    stylesheet.css.should =~ /.winnie .the .pooh/
+    stylesheet.css.should =~ /winnie .the .pooh/
     stylesheet.css.should =~ /color: rgb\(213, 211, 44\)/
     stylesheet.css.should =~ /background-color: rgb\(21, 0, 11\)/
     stylesheet.css.should =~ /font-style: italic/
