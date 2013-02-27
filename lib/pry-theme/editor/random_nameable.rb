400ada260f48278cefde009cf9f141dfffdcafbd
Kyrylo Silin <kyrylosilin@gmail.com>
2013-02-27 10:20:14 +0200

    Editor::RandomNameable: make each Ruby implementation happy

--- lib/pry-theme/editor/random_nameable.rb
+++ lib/pry-theme/editor/random_nameable.rb
@@ -8,7 +8,7 @@ module PryTheme::Editor
     #   generate_random_name #=> "untheatrical"
     #   generate_random_name #=> "genus_leucogenes"
     def generate_random_name
-      part_of_speech = [:adjs, :nouns].sample
+      part_of_speech = [:adjs, :nouns].shuffle!.first
       RandomWord.__send__(part_of_speech).first
     end
     private :generate_random_name
