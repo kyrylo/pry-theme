746e894549dcc655d4341b0cbc63759d16d58444
Kyrylo Silin <kyrylosilin@gmail.com>
2014-05-12 00:50:26 +0300

    WhenStartedHook: warn the user if Pry.config.theme contains gibberish
    Fixes #39 (Unable to load Pry)
    
    There probably should be some tests, but I'm too lazy to figure out how
    to write them properly.

--- lib/pry-theme/when_started_hook.rb
+++ lib/pry-theme/when_started_hook.rb
@@ -4,9 +4,10 @@ module PryTheme
       recreate_user_themes_from_default_ones
       load_themes
 
-      if Pry.config.theme
+      if File.exist?(theme_file)
         ThemeList.activate_theme(Pry.config.theme)
       else
+        display_warning(_pry_) if Pry.config.theme
         ThemeList.activate_theme_intelligently
       end
     end
@@ -15,12 +16,12 @@ module PryTheme
 
     # Copy a default theme to theme directory, but only if it isn't there yet.
     def recreate_user_themes_from_default_ones
-      FileUtils.mkdir_p(USER_THEMES_DIR) unless File.exists?(USER_THEMES_DIR)
+      FileUtils.mkdir_p(USER_THEMES_DIR) unless File.exist?(USER_THEMES_DIR)
       default_themes = Dir.entries(DEF_THEMES_DIR) - %w{. ..}
 
       default_themes.each do |theme|
         user_theme_path = File.join(USER_THEMES_DIR, theme)
-        unless File.exists?(user_theme_path)
+        unless File.exist?(user_theme_path)
           def_theme_path = File.join(DEF_THEMES_DIR, theme)
           FileUtils.cp(def_theme_path, USER_THEMES_DIR)
         end
@@ -34,5 +35,15 @@ module PryTheme
       end
     end
 
+    def theme_file
+      File.join(USER_THEMES_DIR, Pry.config.theme.to_s + PT_EXT)
+    end
+
+    def display_warning(pry)
+      pry.output.puts 'Pry Theme Warning: Pry.config.theme is set to ' +
+        "\"#{ Pry.config.theme }\". There's no such a theme in your system. " +
+        "All installed themes live inside #{ USER_THEMES_DIR }. Falling back " +
+        'to the default theme for now.'
+    end
   end
 end
